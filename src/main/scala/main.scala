b822de1956339b1b77d7260e961c5ac1b8619863
Milo Eiermann <milo.eiermann@gmail.com>
2025-02-17 11:48:02 +0100

    lektion ende commit

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -3,25 +3,47 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 @main
 def main(): Unit =
-  val people3 = List[String] ("dude", "guy", "rando")
-  val people6 = List[String] ("dude", "guy", "rando", "someone", "somebody", "noone")
-  val price = 100
-  val tipPercentage1 = getTipPercentage(people3)
-  val tipPercentage2 = getTipPercentage(people6)
-  println(calculateTip(price, tipPercentage1))
-  println(calculateTip(price, tipPercentage2))
+  // Aufgabe 1
+  //  Regel 1: Nur ein return Wert | Regel 2: Nur abhängikeit zu input | Regel 3: isolation
+  // Aufgabe 1.1: 1: Ja, 2: Nein, 3: Nein || impure
+  // Aufgabe 1.2: 1: Ja, 2: Ja, 3: Ja || pure
+  // Aufgabe 1.3: 1: Ja, 2: Ja, 3: Ja || pure
+  // Aufgabe 1.4: 1: Ja, 2: Nein, 3: Ja || impure
+  // Aufgabe 1.5: 1: Ja, 2: Ja, 3: Ja || pure
+  // Aufgabe 1.6: 1: Ja, 2; Jein, 3: Nein || impure
+  val list = List[Float](1, 2, 3, 4)
+  println(next(list, 0))
+  println(median(list))
 
-def calculateTip(total: Float, tipPercent: Int): Float = {
-  total / 100 * tipPercent
+
+// Aufgabe 2: 1.1
+def addToCart(item: String, cartItems: List[String]): List[String] = {
+  cartItems.appended(item)
+}
+//Aufgabe 2: 1.4
+def multiplyWithRandom(number: Double, randomNum: Double): Double = {
+  number * randomNum
+}
+// Aufgabe 2: 1.6 (not easily solvable: abhängikeit zu konsole)
+def printAndReturnString(str: String): String = {
+  println(str) // not pure
+  str
 }
 
-def getTipPercentage(people: List[String]): Int = {
-  if people.length > 5 then {
-    20
-  } else if people.nonEmpty then {
-    10
-  } else {
-    0
-  }
+// Aufgabe 3.1
+def next(nums: List[Float], total: Float): Float = {
+  if nums.isEmpty then { return total}
+  next(nums.drop(1), nums.head + total)
 }
+
+// Aufgabe 3.2
+def median(nums: List[Float]): Float = {
+  val sorted = nums.sortWith((s: Float, t: Float)
+  => s < t)
+  val middle: Int = sorted.length/2
+  sorted(middle)
+}
+
+
+
 
